0842546810fc095e1b474e409b960edf7c2f1a09
Sudhanshu Pal <palsudhanshu94@gmail.com>
2025-06-10 10:39:11 +0530

    build

--- src/app/custom-pages/company/employee/background-verification/background-verification.component.ts
+++ src/app/custom-pages/company/employee/background-verification/background-verification.component.ts
@@ -252,7 +252,7 @@ export class BackgroundVerificationComponent implements OnInit {
       if (this.formGroup.valid) {
         const formData = new FormData();
         formData.append("emp_id", this.urlId.toString());
-        formData.append("company_id", this.company_id.toString());
+        formData.append("company_id", this.company_id);
         formData.append("documentType", this.selectedDocument);
         formData.append("documentNo", this.f["documentNo"].value);
         formData.append("documentFile", this.selectedImage);
